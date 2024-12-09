0e8b4e48520245df19c69ee390c9a1707031e030
edogola4 <edogola4@gmail.com>
2024-12-09 07:47:39 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import {  Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import  Home  from "./routes/home/home.routes";
-import SignIn  from "./routes/signin/signIn.components";
-import  SignUp from "./routes/signup/signUp.routes";
-import  ProfilePage from "./routes/profilePage/profilePage.components";
+import Home from "./routes/home/home.routes";
+import SignIn from "./routes/signIn/signIn.component";
+import SignUp from "./routes/signUp/signUp.routes";
+import ProfilePage from "./routes/profilePage/profilePage.component";
 import HomePage from "./routes/homePage/homePage.routes";
 import Edit from "./routes/edit/edit.routes";
-import { Route } from 'react-router-dom';
-
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/edit" element={<Edit />} />
-        </Routes>
-    );
-};
-export default App;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/profilepage" element={<ProfilePage />} />
+      <Route path="/homepage/:id" element={<HomePage />} />
+      <Route path="/editprofile/:id" element={<Edit />} />
+    </Routes>
+  );
+}
 
+export default App;
