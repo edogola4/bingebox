f03fe434bb0d519bb609c96fad668f3d3e8abd04
edogola4 <edogola4@gmail.com>
2024-12-13 09:06:01 +0300

    updated

--- src/components/formBase/formBase.component.jsx
+++ src/components/formBase/formBase.component.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+/*import { Link } from "react-router-dom";
 
 import './formBase.styles.scss';
 
@@ -11,10 +11,45 @@ const FormBase = () => {
             </div>
             
             <div className='signup__form'>
-                <p>Already on CinemaEdge? <Link to='/signin'>Sign in</Link></p>
+                <p>Already on BingeBox? <Link to='/signin'>Sign in</Link></p>
             </div>
         </div>        
     )
+}
+
+export default FormBase;
+*/
+
+
+import { Link } from "react-router-dom";
+import './formBase.styles.scss';
+
+const FormBase = () => {
+    return (
+        <div className="form__rest">
+            {/* Remember Me Section */}
+            <div className='remember'>
+                <input 
+                    type='checkbox' 
+                    id='rememberMe' 
+                    className='remember__checkbox' 
+                />
+                <label 
+                    htmlFor='rememberMe' 
+                    className='remember__label'>
+                    Remember my login
+                </label>
+            </div>
+            
+            {/* Sign-up Prompt Section */}
+            <div className='signup__form'>
+                <p className='signup__text'>
+                    Already on BingeBox? 
+                    <Link to='/signin' className='signup__link'>Sign in</Link>
+                </p>
+            </div>
+        </div>        
+    );
 }
 
 export default FormBase;
