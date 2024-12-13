36dc28e1b2fd1df64a99c365575df1473e5f6530
edogola4 <edogola4@gmail.com>
2024-12-13 09:10:12 +0300

    updated

--- src/components/formInput/formInput.component.jsx
+++ src/components/formInput/formInput.component.jsx
@@ -1,14 +1,17 @@
+
 import './formInput.styles.scss';
 
-const FormInput = ({icon, ...otherProps}) => {
+const FormInput = ({ icon, label, errorMessage, ...otherProps }) => {
     return (
         <div className="form-group">
+            {label && <label className="form-group__label">{label}</label>}
             <div className='form-group__input'>
-                <input {...otherProps} />
+                <input {...otherProps} className="form-group__input-field" />
                 {icon && (
                     <div className='form-group__svg'>{icon}</div>
                 )}
             </div>
+            {errorMessage && <p className="form-group__error">{errorMessage}</p>}
         </div>
     )
 }
