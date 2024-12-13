2121b7a4290fe3f380a0d9797b87a38b28171bb2
edogola4 <edogola4@gmail.com>
2024-12-13 09:17:53 +0300

    updated

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -1,13 +1,16 @@
 import './profile.styles.scss';
 
-const Profile = ({image, nickname}) => {
-    return (
-        <div className="profile-body">
-            <div className='profile-body--img'>
-                <img src={image} alt={nickname}/>
-            </div>
-        </div>
-    )
-}
+const Profile = ({ image, nickname }) => {
+  return (
+    <div className="profile-body">
+      <div className="profile-body__img">
+        <img src={image} alt={nickname} />
+      </div>
+      <div className="profile-body__details">
+        <h3 className="profile-body__nickname">{nickname}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
